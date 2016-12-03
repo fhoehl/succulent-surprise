c181ce3d22f0b3f7f32242290f709cfbd5bdcda1
Francois Hoehl <francoishoehl@gmail.com>
2016-12-03 11:57:07 +0000

    Add order model

--- store/models.py
+++ store/models.py
@@ -2,4 +2,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    updated_at = models.DateTimeField('date updated')
+    state = (('I', 'Initiated'), ('S', 'Submitted'), ('F', 'Fulfill')
+             ('D', 'Dispatched'), ('E', 'Error'))
+
+    from_first_name = models.CharField('Sender first name', max_length=60)
+    from_last_name = models.CharField('Sender last name', max_length=60)
+    from_email = models.CharField('Sender email', max_length=100)
+    from_phone = models.CharField('Sender phone', max_length=100)
+
+    to_first_name = models.CharField('Recipient first name', max_length=60)
+    to_last_name = models.CharField('Recipient last name', max_length=60)
+
+    delivery_company_name = models.CharField('Company name', max_length=60)
+    delivery_first = models.CharField('Address line 1', max_length=100)
+    delivery_second = models.CharField('Address line 2', max_length=100)
+    delivery_third = models.CharField('Address line 3', max_length=100)
+    delivery_postcode = models.CharField('Postcode', max_length=10)
+    delivery_note = models.CharField('Gift note', max_length=200)
--- succulent_surprise_service/settings.py
+++ succulent_surprise_service/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'store.apps.StoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'succulent_surprise_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'succulent',
+        'USER': 'succulent',
+        'PASSWORD': 'super',
+        'HOST': '127.0.0.1',
+        'PORT': '32768',
     }
 }
 
