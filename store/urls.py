4723d10b5925a58b35195f5ab26aa0fdc1c5bddf
Francois Hoehl <francoishoehl@gmail.com>
2016-12-05 20:37:29 +0000

    Add checkout view

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^order/$', views.OrderCreate.as_view(), name='order'),
+    url(r'^checkout/$', views.OrderCreate.as_view(), name='checkout'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+
+from store.models import Order
+
+class OrderCreate(CreateView):
+    model = Order
+    http_method_names = ['get', 'post']
+    success_url = '/thanks/'
+    fields = ['from_first_name', 'from_last_name', 'from_email', 'from_phone', 'to_first_name', 'to_last_name', 'delivery_company_name', 'to_first_name']
+    #template_name = 'store/checkout.html'
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, 'store/index.html')
+
+def checkout(request):
+    return render(request, 'store/checkout.html')
