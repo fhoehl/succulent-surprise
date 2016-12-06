47c976d90bd36bdb153aef9507c8e64283fe9e57
Francois Hoehl <francoishoehl@gmail.com>
2016-12-06 13:26:08 +0000

    Update checkout and thanks page
    Add social buttons
    Fix problems with input
    Tweak styles

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^thanks/$', views.thanks, name='thanks'),
     url(r'^tos/$', views.tos, name='tos'),
-    url(r'^checkout/$', views.OrderCreate.as_view(), name='checkout'),
+    url(r'^checkout/$', views.checkout, name='checkout'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic.edit import CreateView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.edit import CreateView, ModelFormMixin
+from django.forms import Form, ModelForm
 
 from store.models import Order
 
@@ -8,7 +9,43 @@ class OrderCreate(CreateView):
     model = Order
     http_method_names = ['get', 'post']
     success_url = '/thanks/'
-    fields = ['from_first_name', 'from_last_name', 'from_email', 'from_phone', 'to_first_name', 'to_last_name', 'delivery_company_name', 'to_first_name']
+    fields = ['from_first_name', 'from_last_name', 'from_email', 'to_first_name', 'to_last_name', 'delivery_company_name', 'to_first_name']
+
+class CheckoutForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = ['from_first_name', 'from_last_name',
+                  'to_first_name', 'to_last_name',
+                  'delivery_company_name', 'delivery_first', 'delivery_second',
+                  'delivery_third', 'delivery_postcode', 'delivery_note']
+
+def checkout(request):
+    if request.method == 'POST':
+        form = CheckoutForm(request.POST)
+
+        if form.is_valid():
+            print form
+            print form.cleaned_data
+            order = Order(from_first_name=form.cleaned_data['from_first_name'],
+                          from_last_name=form.cleaned_data['from_last_name'],
+                          from_email=request.POST.get('stripeEmail'),
+                          stripe_tx=request.POST.get('stripeToken'),
+                          to_first_name=form.cleaned_data['to_first_name'],
+                          to_last_name=form.cleaned_data['to_last_name'],
+                          delivery_company_name=form.cleaned_data['delivery_company_name'],
+                          delivery_first=form.cleaned_data['delivery_first'],
+                          delivery_second=form.cleaned_data['delivery_second'],
+                          delivery_third=form.cleaned_data['delivery_third'],
+                          delivery_postcode=form.cleaned_data['delivery_postcode'],
+                          delivery_note=form.cleaned_data['delivery_note'],
+                         )
+            order.save()
+
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = CheckoutForm()
+
+    return render(request, 'store/order_form.html', {'form': form})
 
 def index(request):
     return render(request, 'store/index.html')
